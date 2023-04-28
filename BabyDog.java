9eb473567a2c0e5a049c8cf87a361d78b48dead8
Mrunali <mrunaliurade26@gmail.com>
2023-04-28 22:46:32 +0530

    example of polymorphism

---
+++ BabyDog.java
@@ -0,0 +1,25 @@
+class Animal
+ {  
+		void eat(){System.out.println("eating");}  
+		}  
+		  
+class Dog extends Animal{  
+		void eat(){System.out.println("eating fruits");}  
+		}  
+		  
+public class BabyDog extends Dog {
+	
+		 
+		void eat(){System.out.println("drinking milk");}  
+		  
+		public static void main(String args[]){  
+		Animal a1;  
+		a1=new Animal();  
+		a1.eat();  
+		a1=new Dog();  
+		a1.eat();  
+		a1=new BabyDog();  
+		a1.eat();  
+		}  
+		}  
+
