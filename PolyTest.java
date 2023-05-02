6a6af48e3c0c912c573cc95a87bf37b1e46ec2fa
Mrunali Urade <124710812+UradeMrunali@users.noreply.github.com>
2023-05-02 21:43:37 +0530

    Update PolyTest.java

--- PolyTest.java
+++ PolyTest.java
@@ -3,11 +3,9 @@ public class PolyTest {
  public static void main(String[] args) { 
 	Poly3 p =new Poly3();
 	p.display();
-	
 //upcasting : When reference variable of Parent class refers to the object of Child class
 	Poly2 p1 =new Poly3();
 	p1.display();
-	
 //downcasting or manual casting is not possible directly you have to apply filter like "Poly3"	
 	Poly3 p2 =(Poly3) new Poly2();
 	p2.display();
